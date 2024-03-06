f828304cc1affc7ff3ba5832d0036ec1c91b5d3f
ZahraAgha <zahrasha@code.edu.az>
2024-03-06 09:57:53 +0400

    commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Card from './Card';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>This Title</h1>
+      <Card />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Info.js
@@ -0,0 +1,14 @@
+// Info.js
+import React from 'react';
+
+const Info = () => {
+    return (
+        <div className="info">
+            <img src="https://kinsta.com/wp-content/uploads/2022/06/what-is-react-js-feature-image-1200x675.png" alt="Product Image" />
+            <h2>Title</h2>
+            <p>Price: $19.99</p>
+        </div>
+    );
+};
+
+export default Info;
